51dcff8f5f8cdab87fca0a5f8a78afa925fabadc
67yyy67 <pau.ramirez.09@ipsi.cat>
2025-10-24 12:33:07 +0200

    ss

---
+++ test.py
@@ -0,0 +1,30 @@
+tolerancia = 10**(-5)
+m = [1, -1]
+m.sort()
+def f(x):
+    return 2*x + 1
+while True:
+    if abs(m[0] - m[1]) <= tolerancia:
+        print(f"La funcio definida es 0 a {f(m[1])}")
+        break
+    if abs(f(m[0])) <= tolerancia:
+        print(f"La funcio definida es 0 a {f(m[0])}")
+        break
+    elif abs(f(m[1])) <= tolerancia:
+        print(f"La funcio definida es 0 a {f(m[1])}")
+        break
+    elif f(m[0])*f(m[1]) < 0:
+        x = (m[0] + m[1])/2
+        if abs(f(x)) <= tolerancia:
+            print(f"La funcio definida es 0 a {f(x)}")
+            break
+        elif f(x)*f(m[0]) < 0:
+            m[1] = x
+            continue
+        elif f(x)*f(m[1]) < 0:
+            m[0] = x
+            continue
+    else:
+        print("no hi ha cap 0 a l'interval definit")
+        break
+
