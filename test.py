3bfeba80f4b77d79b814629b11ee9b3262b00a43
67yyy67 <pau.ramirez.09@ipsi.cat>
2025-10-27 13:48:23 +0100

    XD

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 tolerancia = 10**(-5)
-m = [1, -1]
-m.sort()
+m = [1, -1]     
 def f(x):
     return 2*x + 1
 while True:
     if abs(m[0] - m[1]) <= tolerancia:
         print(f"La funcio definida es 0 a {f(m[1])}")
         break
-    if abs(f(m[0])) <= tolerancia:
+    elif abs(f(m[0])) <= tolerancia:
         print(f"La funcio definida es 0 a {f(m[0])}")
         break
     elif abs(f(m[1])) <= tolerancia:
@@ -16,7 +15,7 @@ while True:
     elif f(m[0])*f(m[1]) < 0:
         x = (m[0] + m[1])/2
         if abs(f(x)) <= tolerancia:
-            print(f"La funcio definida es 0 a {f(x)}")
+            print(f"La funcio definida es 0 a {x}")
             break
         elif f(x)*f(m[0]) < 0:
             m[1] = x
