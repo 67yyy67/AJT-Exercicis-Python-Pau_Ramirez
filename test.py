7d31b8e261b017e407f63269925073017797b7af
67yyy67 <pau.ramirez.09@ipsi.cat>
2025-11-03 14:00:04 +0100

    qqq

--- test.py
+++ test.py
@@ -1,29 +1,13 @@
-tolerancia = 10**(-5)
-m = [1, -1]     
-def f(x):
-    return 2*x + 1
-while True:
-    if abs(m[0] - m[1]) <= tolerancia:
-        print(f"La funcio definida es 0 a {f(m[1])}")
-        break
-    elif abs(f(m[0])) <= tolerancia:
-        print(f"La funcio definida es 0 a {f(m[0])}")
-        break
-    elif abs(f(m[1])) <= tolerancia:
-        print(f"La funcio definida es 0 a {f(m[1])}")
-        break
-    elif f(m[0])*f(m[1]) < 0:
-        x = (m[0] + m[1])/2
-        if abs(f(x)) <= tolerancia:
-            print(f"La funcio definida es 0 a {x}")
-            break
-        elif f(x)*f(m[0]) < 0:
-            m[1] = x
-            continue
-        elif f(x)*f(m[1]) < 0:
-            m[0] = x
-            continue
-    else:
-        print("no hi ha cap 0 a l'interval definit")
-        break
+n = int(input("introdueix un nombre enter més gran o igual que 0"))
+Triangle = []
+for a in range(0, n):
+    Triangle.append([])
+    Triangle[a].append(1)
+    for b in range(0,a):
+        Triangle[a].append(Triangle[a-1][b-1]+Triangle[a-1][b])
+    Triangle[a].append(1)
+
+
+
+print(Triangle)
 
