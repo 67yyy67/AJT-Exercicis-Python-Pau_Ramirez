85b85abf226be5a0b2fcbe90e24c9fde8db41c54
67yyy67 <pau.ramirez.09@ipsi.cat>
2025-12-15 14:04:26 +0100

    wsw

--- Untitled-1.py
+++ Untitled-1.py
@@ -16,28 +16,21 @@ def detmat(A):
             for a in range(len(NovA)):
                 Bij[a].remove(Bij[a][j])
             return Bij
-        #funció per expandir la matriu Aij a A. A es A i B es Aij 
-        def funciomatdetAijaA(B, A, i, j):
-            Cij = (B[0][0]*B[1][1]+B[0][1]*B[1][0])*(-1)**(i + j)
-            detCij = A[i][j]*Cij
-            return detCij
-        while True:
-            if len(NovA) == 2:
-                detA = NovA[0][0]*NovA[1][1]+NovA[0][1]*NovA[1][0]
-                break
-            elif len(NovA) == 3:
-                for a in range(len(A)):
-                    for b in range(len(A[a])):
-                        detA = detA + funciomatdetAijaA(funciomatAij(A, a, b), A, a, b)
-                break
+        
+        def funciodetmat(A):
+            if len(A) == 1:
+                detA = A[0][0]
             else:
-                pass          
+                for i in range(len(A)):
+                    detA += (funciodetmat(funciomatAij(A, i, 0)))*(-1)**(i + 0)
+            return detA   
     return detA
 
 
                     
             
-x = [[2, 3, 4],
-     [2, 2, 2],
-     [2, 3, 4],]
+x = [[2, 3, 4, 5],
+     [2, 2, 2, 5],
+     [2, 3, 4, 5],
+     [2, 3, 4, 5]]
 print(detmat(x))
