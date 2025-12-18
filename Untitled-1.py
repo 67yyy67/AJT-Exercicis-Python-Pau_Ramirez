7a968c024399966ab4b1c4fe8c16a5aa8ca43a4f
67yyy67 <pau.ramirez.09@ipsi.cat>
2025-12-18 10:56:15 +0100

    sds

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,36 +1,68 @@
-def detmat(A):
-    if len(A) != len(A[0]):
-        print("no determinable")
-    elif len(A) < 2:
-        detA = A[0][0]
+A = [
+    ["-","-","-"],
+    ["-","-","-"],
+    ["-","-","-"]
+    ]
+jugador = "X"
+def canvijug(a):
+    if a == "X":
+        b = "O"
     else:
-        NovA = A
-        Aij = []
-        detAij = []
-        currentAij = A
-        detA = 0
-        #funció per conseguir Aij, traient la columna j  i la fila i de la matriu
-        def funciomatAij(B, i, j):
-            Bij = B
-            Bij.remove(Bij[i])
-            for a in range(len(NovA)):
-                Bij[a].remove(Bij[a][j])
-            return Bij
-        
-        def funciodetmat(A):
-            if len(A) == 1:
-                detA = A[0][0]
-            else:
-                for i in range(len(A)):
-                    detA += (funciodetmat(funciomatAij(A, i, 0)))*(-1)**(i + 0)
-            return detA   
-    return detA
+        b = "X"
+    return b
+def mostramat(A):
+    for a in range(len(A)):
+        print(A[a])
+def posarfitxa(A, jug):
+    try: 
+        a = int(input("a quina fila vols posar fitxa?"))
+        b = int(input("a quina columna vols posar fitxa?"))
+        if ((a or b) not in range(1, 4)) or A[a-1][b-1] != "-":
+            print("posició no válida")
+            return posarfitxa(A, jug)
+        else:
+            A[a-1][b-1] = jug
+    except:
+        print("posició no válida")
+        return posarfitxa(A, jug)
+def clear(A):
+    for a in range(len(A)):
+        for b in range(len(A[a])):
+            A[a][b] = "-"
+punts = {"O" : 0, "X" : 0, "-" : 0}
+while True:
+    final = False
+    print("--------------")
+    posarfitxa(A, jugador)
+321    mostramat(A)
+    for a in range(len(A)):
+        if A[a][0] == A[a][1] and A[a][2] == A[a][1] and A[a][0] != "-":
+            print(f"ha guanyat el jugador {jugador}")
+            final = True
+            vic = f"{jugador}"
+        elif A[0][a] == A[1][a] and A[1][a] == A[2][a] and A[0][a] != "-":
+            print(f"ha guanyat el jugador {jugador}")
+            final = True
+            vic = f"{jugador}"
+    if (A[0][0] == A[1][1] and A[1][1] == A[2][2]) or (A[2][0] == A[1][1] and A[1][1] == A[0][2]) and A[1][1] != "-":
+        print(f"ha guanyat el jugador {jugador}")
+        final = True
+        vic = f"{jugador}"
+    if "-" not in (A[0] and A[1] and A[2]):
+        print(f"Empat")
+        final = True
+        vic = "-"
+    if final == True:
+        input_seguir = input("Voleu seguir jugant? Y/N")
+        punts[vic] += 1
+        print(f"X ha guanyat {punts["X"]} cops, O ha guanyat {punts["O"]} cops, i heu empatat {punts["-"]} cops")
+        if input_seguir == "Y":
+            clear(A)
+            jugador = "X"
+            continue
+        else:
+            print("moltes gracies per jugar")
+            break
+    jugador = canvijug(jugador)
 
 
-                    
-            
-x = [[2, 3, 4, 5],
-     [2, 2, 2, 5],
-     [2, 3, 4, 5],
-     [2, 3, 4, 5]]
-print(detmat(x))
